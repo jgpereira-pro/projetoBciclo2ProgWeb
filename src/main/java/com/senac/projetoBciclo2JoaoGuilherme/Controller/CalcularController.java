38825e602bc1d4b7ae796918039469b1db053869
cavera <jgpereirafreitas@gmail.com>
2025-12-05 19:46:55 -0300

    terceito

--- src/main/java/com/senac/projetoBciclo2JoaoGuilherme/Controller/CalcularController.java
+++ src/main/java/com/senac/projetoBciclo2JoaoGuilherme/Controller/CalcularController.java
@@ -3,18 +3,21 @@ package com.senac.projetoBciclo2JoaoGuilherme.Controller;
 import com.senac.projetoBciclo2JoaoGuilherme.dto.LadoDTO;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller
+@RestController
 public class CalcularController {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @PostMapping("/calcular")
-    public void calcularLado(LadoDTO ladoDTO){
-        rabbitTemplate.convertAndSend("provac2JoaoGuilherme", ladoDTO);
+    public Double calcularLado(@RequestBody LadoDTO ladoDTO){
+        Object resposta = rabbitTemplate.convertSendAndReceive("provaciclo2pw1noite", "provac2JoaoGuilherme", ladoDTO);
+
+        return (Double) resposta;
     }
 
 }
